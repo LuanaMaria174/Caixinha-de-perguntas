cfadde509e9c706a428e5de8c8c4942664b4d832
LuanaMaria174 <luana.maria.araujo@escola.pr.gov.br>
2025-09-23 09:10:54 -0300

    .

---
+++  Perguntas.js
@@ -0,0 +1,25 @@
+CriaCartão(
+    categoria,
+    pergunta,
+    resposta,
+)
+CriaCartão(
+   'Deus?',
+   ' A Santíssima Trindade, são um só Deus? Ou eles são distintos',
+   'Sim! Eles são um só Deus, porém Deus Pai não é filho, nem Espirito Santo, nem o Espirito Santo é Pai e filho, muito menos o Filho é Pai ou Espirito Santo.'
+)
+CriaCartão(
+    'Poder?',
+    'Um é mais poderoso que o outro?', 
+    'Não, todos tem o mesmo poder absoluto, Onipotente, onisciente e onipresente, ninguém é maior que ninguém entre si.'
+)
+CriaCartão(
+    'deusa?',
+    'Maria é deusa?',
+    'ABSOLUTAMETE NÃO! Maria é santa e não deusa, qualquer outro ser que for colocado assima de DEus ou comparativo a ele é um pecado gravissimo, totalmente negado pela igreja católica,  apostólica romana.'
+)
+CriaCartão(
+    'Perdão',
+    'Deus não perdoa pecados graves e repetitivos?',
+    'Não, idependente da gravidade do pecado, e a quantidade de vezes cometida, o coração de Deus é infinitamente misericordioso para conosco, e desconfiar da sua misericórdia o machuca mais que cometeter um pecado mortal'
+)
